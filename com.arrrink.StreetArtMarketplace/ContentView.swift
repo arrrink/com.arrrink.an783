a4fb3e26552c6aded82739c8f9f4399bf0d33427
arrrink <arina.nefa@gmail.com>
2020-08-19 00:06:06 +0300

    Initial Commit

---
+++ com.arrrink.StreetArtMarketplace/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  com.arrrink.StreetArtMarketplace
+//
+//  Created by Арина Нефёдова on 19.08.2020.
+//  Copyright © 2020 Арина Нефёдова. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
